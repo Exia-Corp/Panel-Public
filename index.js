4428742f8966f593c3eb5c03b976b9ad4609eb73
Neoned is Here 〆 <neonedishere@gmail.com>
2025-09-08 09:12:08 +0200

    Mise en place de la page 404

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 document.getElementById("invite-btn").addEventListener("click", () => {
 	window.location.href = `https://discord.com/oauth2/authorize?client_id=1265435365280448603&permissions=8&integration_type=0&scope=bot+applications.commands`
 });
+
+document.getElementById('login-btn').addEventListener("click", () => {
+	window.location.href = 'https://discord.com/oauth2/authorize?client_id=1280974699735814206&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdashboard%2Fauth%2Fcallback&scope=identify+email+connections'
+})
