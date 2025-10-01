5bbfe4e5a5f2154fe4db99f49306900d829155c9
Neoned is Here 〆 <neonedishere@gmail.com>
2025-10-01 11:17:48 +0200

    .

---
+++ script/404.js
@@ -0,0 +1,18 @@
+const params = new URLSearchParams(window.location.search);
+const userId = params.get('user');
+
+if (!userId) {
+	document.body.innerHTML = `<h2>Utilisateur non identifié</h2>`;
+} else {
+	fetch(`http://localhost:3000/dashboard/auth/user/${userId}`)
+		.then(res => res.json())
+		.then(data => {
+			document.getElementById('username').textContent = data.username;
+            const avatarExtension = data.avatar.startsWith('a_') ? 'gif' : 'png';
+			document.getElementById('avatar').src = `https://cdn.discordapp.com/avatars/${data.discord_id}/${data.avatar}.${avatarExtension}`;
+		})
+		.catch(err => {
+			console.error(err);
+			document.body.innerHTML = '<h2>Erreur lors du chargement des ressources</h2>';
+		});
+}
--- script/home.js
+++ script/home.js
@@ -4,19 +4,49 @@ const userId = params.get('user');
 if (!userId) {
 	document.body.innerHTML = `<h2>Utilisateur non identifié</h2>`;
 } else {
-	fetch(`http://localhost:1336/dashboard/auth/user/${userId}`)
+	console.log("userId récupéré :", userId);
+
+	fetch(`http://localhost:3000/dashboard/auth/user/${userId}`, {
+		method: "GET",
+		headers: {
+			"Content-Type": 'application/json',
+			"Authorization": 'Bearer KEY-9e2e9b18fcb108ff9435-API'
+		}
+	})
 		.then(res => res.json())
 		.then(data => {
-			document.getElementById('username').textContent = data.username;
-            const avatarExtension = data.avatar.startsWith('a_') ? 'gif' : 'png';
-			document.getElementById('avatar').src = `https://cdn.discordapp.com/avatars/${data.discord_id}/${data.avatar}.${avatarExtension}`;
+			console.log(data)
+
+			const usernameEl = document.querySelector('#username');
+			const avatarEl = document.querySelector('#avatar');
+
+			if (usernameEl) {
+				if (data.username) {
+					usernameEl.textContent = data.username;
+				} else {
+					usernameEl.textContent = 'discord'
+				}
+			}
+
+			if (avatarEl) {
+				if (data.avatar) {
+					const avatarExtension = data.avatar.startsWith('a_') ? 'gif' : 'png';
+					avatarEl.src = `https://cdn.discordapp.com/avatars/${data.user_id}/${data.avatar}.${avatarExtension}`;
+				} else {
+					avatarEl.src = `https://cdn.discordapp.com/embed/avatars/0.png`;
+				}
+			}
 		})
 		.catch(err => {
 			console.error(err);
-			document.body.innerHTML = '<h2>Erreur lors du chargement des ressources</h2>';
+			document.body.innerText = 'Erreur lors du chargement des ressources';
 		});
 }
 
-document.getElementById('invite-btn').addEventListener('click', () => {
-	window.location.href = "https://discord.com/oauth2/authorize?client_id=1265435365280448603&permissions=8&integration_type=0&scope=bot+applications.commands";
-})	
+const inviteBtn = document.getElementById('invite-btn');
+if (inviteBtn) {
+	inviteBtn.addEventListener('click', () => {
+		window.location.href =
+			"https://discord.com/oauth2/authorize?client_id=1265435365280448603&permissions=8&integration_type=0&scope=bot+applications.commands";
+	});
+}
